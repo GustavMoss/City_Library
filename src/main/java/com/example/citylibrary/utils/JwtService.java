162a7fc7b6e015c484c0bdd8792703e6c473cafc
Martin Leo <martin.christoffer.leo@gmail.com>
2024-11-19 17:54:53 +0100

    kept building on this, seems like some parts of the jwt package has been outdated/deprecated so might have to look into the more up-to-date solution

--- src/main/java/com/example/citylibrary/utils/JwtService.java
+++ src/main/java/com/example/citylibrary/utils/JwtService.java
@@ -1,11 +1,28 @@
 package com.example.citylibrary.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "myverySecretKey";
+
     public String extractUsername(String jwtToken) {
         return null;
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(jwtToken).getPayload();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

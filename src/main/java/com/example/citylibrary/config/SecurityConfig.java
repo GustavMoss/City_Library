57a3939e49d8b8e055f6eb05af5924b9f30d6344
Martin Leo <martin.christoffer.leo@gmail.com>
2024-11-27 13:36:04 +0100

    added a very basic config for spring security. A lot of stuff needs changing or more work. Also added password encryption, only for users though.

---
+++ src/main/java/com/example/citylibrary/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.citylibrary.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(auth ->
+                {
+
+                    try {
+                        auth
+                                .requestMatchers("/users").permitAll() // this should not be open to everyone probably, make a new endpoint specifically for register probably.
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .anyRequest().authenticated().and().headers().frameOptions().sameOrigin(); // and().headers().frameOptions().sameOrigin() is to get h2-console to work. Have to set the X-Frame something header otherwise the browser will complain. most of them are deprecated and I have to wrap in a try/catch since the .headers throws an error. there is probably an easier or prettier way than this.
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+
+                .csrf(AbstractHttpConfigurer::disable) // have to disable csrf for the h2-console to work, get forbidden otherwise.
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    // this will compare passwords as far as I understand. The daoauth provider is needed to get the info from database
+    // and then we set the passwordencoder and set a userdetailsservice that we've customized to fit our need. In this to get the email instead of username even though it's called by username in the userdetails interface, so a bit confusing.
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
+    }
+}
--- src/main/java/com/example/citylibrary/user/UserService.java
+++ src/main/java/com/example/citylibrary/user/UserService.java
@@ -3,6 +3,7 @@ package com.example.citylibrary.user;
 import com.example.citylibrary.exceptions.LibBadRequest;
 import com.example.citylibrary.loan.Loans;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,10 @@ public class UserService {
 
     private final UserRepository userRepo;
 
+    // init the password encoder, built into spring security. Used to encode password with bcrypt, the strength is the number of rounds
+    // seems very simple to implement.
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     @Autowired
     public UserService(UserRepository userRepo) {
         this.userRepo = userRepo;
@@ -20,6 +25,8 @@ public class UserService {
 
     // create new user
     public Users createNewUser(Users user) {
+        // this just runs the password through the encoder and sets the generated hash to the password before saving the user to db.
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
